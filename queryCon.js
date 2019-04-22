32daa359de31b564ba3c172e57ed5ede8cd5efe8
BlacZman <37856140+BlacZman@users.noreply.github.com>
2019-04-23 02:27:32 +0700

    Better performance with less chance to be overflow
    Prevent user from spamming search query
    Change connection of mysql from one-to-one to pool
    (Need to test to determine impact)

--- queryCon.js
+++ queryCon.js
@@ -2,68 +2,73 @@ exports.connectquery =  {
     con : null,
     createquery : function(user, pass) {
         var mysql = require('mysql');
-        con = mysql.createConnection({
+        con = mysql.createPool({
             host: "localhost",
             user: user,
             password: pass,
             database: "kmuttbookstore"
         });
-        console.log("Connected to '" + user + "'");
+        console.log("Server> Connected to '" + user + "'");
     },
     createDB : function(name) {
         if(con == null) throw "Error: not connect to mysql";
-        con.connect(function(err) {
+        con.getConnection(function(err, connection) {
             if(err) throw err;
             con.query("CREATE DATABASE " + name, function (err, result) {
                 if (err) throw err;
                 console.log("Database " + name + " created");
+                connection.release();
             });
         });
     },
     deleteDB: function(name) {
         if(con == null) throw "Error: not connect to mysql";
-        con.connect(function(err) {
+        con.getConnection(function(err, connection) {
             if(err) throw err;
             con.query("DELETE DATABASE " + name, function (err, result) {
                 if (err) throw err;
                 console.log("Database " + name + " deleted");
+                connection.release();
             });
         });
     },
     createTable: function(name, properties) {
         if(con == null) throw "Error: not connect to mysql";
-        con.connect(function(err) {
+        con.getConnection(function(err, connection) {
             if(err) throw err;
             con.query("CREATE TABLE " + name + " (" + properties + ")", function (err, result) {
                 if (err) throw err;
                 console.log("Table " + name + " created");
+                connection.release();
             });
         });
     },
     deleteTable:  function(name) {
         if(con == null) throw "Error: not connect to mysql";
-        con.connect(function(err) {
+        con.getConnection(function(err, connection) {
             if(err) throw err;
             con.query("DROP TABLE " + name, function (err, result) {
                 if (err) throw err;
                 console.log("Table " + name + " deleted");
+                connection.release();
             });
         });
     },
     insertTable: function(table, keys, values) {
         if(con == null) throw "Error: not connect to mysql";
-        con.connect(function(err) {
+        con.getConnection(function(err, connection) {
             if(err) throw err;
             con.query("INSERT INTO " + table + " (" + keys + ") VALUES (" + values + ")", function (err, result) {
                 if (err) throw err;
                 console.log("Keys: " + keys + " Values: " + values + " added to " + table);
+                connection.release();
             });
         });
     },
     //queryResult: [{isbn:"111"}],
     selector: function(table, prop, whrcommand, passValue_callback) { //selection(tb, prop, cmd, function(err, result) {})
         if(con == null) throw "Error: not connect to mysql";
-        con.connect(function(err) {
+        con.getConnection(function(err, connection) {
             if(err) throw err;
             con.query("SELECT " + prop + " FROM " + table + " WHERE " + whrcommand, function (err, result, field) {
                 if (err) throw err;
@@ -76,12 +81,13 @@ exports.connectquery =  {
                     childarr.push(op);
                 });
                 passValue_callback(null, childarr);
+                connection.release();
             });
         });
     },
     selectAll: function(table, passValue_callback) {
         if(con == null) throw "Error: not connect to mysql";
-        con.connect(function(err) {
+        con.getConnection(function(err, connection) {
             if(err) throw err;
             con.query("SELECT * FROM " + table, function (err, result, field) {
                 if (err) throw err;
@@ -94,6 +100,7 @@ exports.connectquery =  {
                     childarr.push(op);
                 });
                 passValue_callback(null, childarr);
+                connection.release();
             });
         });
     }
--- server.js
+++ server.js
@@ -5,40 +5,40 @@ mysql = require('mysql');
 myquery = require("./queryCon"),
 dec = require('atob'),
 account = require('./database');
-pass = account.data.pass//your password,
-mydb = myquery.connectquery,
+pass = account.data.pass,//your password
+
 express = require('express'),
 app = express();
 app.use(express.static('pic'));
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
     fs.readFile('index.html', function(err, data) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(data);
+        res.end();
+    });
+});
+app.get('/?*', function(req, res) {
+    setTimeout(()=>{
         var query = url.parse(req.url, true).query;
         var arr = [];
-        if(query.search == null) {
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.write(data);
-            res.end();
-        }
-        else {
-            var txtstr = "<div id=\"parentDiv\">";
-            mydb.createquery(account.data.user/*your username*/, dec(account.data.pass)/*decoding password if it's encoded*/);
-            mydb.selector("book", "isbn, title, author, translator, publisher, price, timestamp", "(isbn LIKE '%" + query.search + "%' OR title LIKE '%" + query.search + "%' OR author LIKE '%" + query.search + "%' OR translator LIKE '%" + query.search + "%' OR publisher LIKE '%" + query.search + "%')", function(err, result) {
-                if(err) throw err;
-                arr = result;
-                Object.keys(arr).forEach(function(key) {
-                    var row = arr[key];
-                    console.log("Search for " + query.search + " Result: " + row.isbn);
-                    txtstr = txtstr + "<div id=\"Bookcontent\"><img id=\"pic\" src=\"" + row.isbn + ".jpg\" alt=\"No picture\" onError=\"this.onerror=null;this.src='404.jpg'\"><p>ISBN: " + row.isbn + ", Title: " + row.title + ", Author: " + row.author + ", Translator: " + row.translator + ", Publisher: " + row.publisher + ", Price: " + row.price + ", Timestamp: " + row.timestamp + "</p></div>";
-                });
-                txtstr = txtstr + "</div>";
+        var txtstr = "<div id=\"parentDiv\">";
+        mydb = myquery.connectquery;
+        mydb.createquery(account.data.user/*your username*/, dec(account.data.pass)/*decoding password if it's encoded*/);
+        mydb.selector("book", "isbn, title, author, translator, publisher, price, timestamp", "(isbn LIKE '%" + query.search + "%' OR title LIKE '%" + query.search + "%' OR author LIKE '%" + query.search + "%' OR translator LIKE '%" + query.search + "%' OR publisher LIKE '%" + query.search + "%')", function(err, result) {
+            if(err) throw err;
+            arr = result;
+            Object.keys(arr).forEach(function(key) {
+                var row = arr[key];
+                console.log("Search for " + query.search + " Result: " + row.isbn);
+                txtstr = txtstr + "<div id=\"Bookcontent\"><img id=\"pic\" src=\"" + row.isbn + ".jpg\" alt=\"No picture\" onError=\"this.onerror=null;this.src='404.jpg'\"><p>ISBN: " + row.isbn + ", Title: " + row.title + ", Author: " + row.author + ", Translator: " + row.translator + ", Publisher: " + row.publisher + ", Price: " + row.price + ", Timestamp: " + row.timestamp + "</p></div>";
             });
-            setTimeout(()=>{ res.writeHead(200, {"Content-Type": "text/html"});
-                console.log(txtstr);
-                res.write("Result for " + mysql.escape(query.search) + " is:");
-                if(txtstr != "<div id=\"parentDiv\"></div>")res.end(txtstr);
-                else res.end("<p>No result</p>");
-            }, 100);
-        }
-    });
+            txtstr = txtstr + "</div>";
+        });
+        setTimeout(()=>{ res.writeHead(200, {"Content-Type": "text/html"});
+            res.write("Result for " + mysql.escape(query.search) + " is:");
+            if(txtstr != "<div id=\"parentDiv\"></div>")res.end(txtstr);
+            else res.end("<p>No result</p>");
+        }, 100);
+    },300);
 });
 var server = app.listen(8080);
